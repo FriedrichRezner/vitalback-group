eaa512d334dfd86be7b444d272aed80fdd83cd37
friedrich.rezner <friedrich.rezner@aoe.com>
2021-03-18 14:07:15 +0100

    Add footer and contact form

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -35,3 +35,12 @@ $('.js-fancybox').each(function () {
     var fancybox = $.HSCore.components.HSFancyBox.init($(this));
 });
 
+/* Contact Map */
+
+$('#map').each(function () {
+    var leaflet = $.HSCore.components.HSLeaflet.init($(this)[0]);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        id: 'mapbox/light-v9'
+    }).addTo(leaflet);
+});
