c9f816c806c75c5a691df8ee48913c076b465536
friedrich.rezner <friedrich.rezner@aoe.com>
2021-08-23 19:40:06 +0200

    Add "Leistungen" page and adjust group header

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,34 +1,3 @@
-/* Scroll Nav Start */
-
-var header = new HSHeader($('#header')).init();
-var isClosed = false;
-
-$('.js-scroll-nav').each(function () {
-    new HSScrollNav($(this), {
-        beforeShow: function () {
-            if (window.innerWidth < 768) {
-                $('#navBar').collapse('hide').on('hidden.bs.collapse', function () {
-                    isClosed = true;
-                });
-            } else {
-                isClosed = true;
-            }
-
-            return isClosed;
-        },
-        afterShow: function () {
-            isClosed = false;
-        }
-    }).init();
-});
-
-AOS.init({
-    duration: 650,
-    once: true
-});
-
-/* Scroll Nav End */
-
 /* Fancybox */
 
 $('.js-fancybox').each(function () {
